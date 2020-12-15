e9a7de5c3b0256f61d756176665bb31e0752b661
harshmeet30 <66329612+harshmeet30@users.noreply.github.com>
2020-12-15 17:11:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,4 @@
-# USAGE
-# Start the server:
-# 	python run_keras_server.py
-# Submit a request via cURL:
-# 	curl -X POST -F image=@dog.jpg 'http://localhost:5000/predict'
-# Submita a request via Python:
-#	python simple_request.py
 
-# import the necessary packages
 from keras.applications import ResNet50
 from keras.preprocessing.image import img_to_array
 from keras.applications import imagenet_utils
@@ -20,9 +12,7 @@ app = flask.Flask(__name__)
 model = None
 
 def load_model():
-	# load the pre-trained Keras model (here we are using a model
-	# pre-trained on ImageNet and provided by Keras, but you can
-	# substitute in your own networks just as easily)
+	# load the pre-trained Keras model (
 	global model
 	model = ResNet50(weights="imagenet")
 
@@ -74,10 +64,8 @@ def predict():
 	# return the data dictionary as a JSON response
 	return flask.jsonify(data)
 
-# if this is the main thread of execution first load the model and
-# then start the server
+# start the server
 if __name__ == "__main__":
-	print(("* Loading Keras model and Flask starting server..."
-		"please wait until server has fully started"))
+	print(("* Loading Keras model and Flask starting server..."))
 	load_model()
 	app.run()
